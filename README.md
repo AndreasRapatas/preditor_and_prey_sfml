c01c4b56cac3762594ce7dfdc07109a02a2bf434
Andy Rapatas <15367779+Rapatas@users.noreply.github.com>
2019-02-25 08:59:02 +0200

    Added installation target with CLI arguments

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # Predator & Prey
-Predator & Prey is a [cellular automaton](https://en.wikipedia.org/wiki/Cellular_automaton) simulating an environment where a predator (red) and a prey (green) exist in a harmonic balance using the following set of rules:
+This project implements the [cellular automaton](https://en.wikipedia.org/wiki/Cellular_automaton) Predator & Prey using C++ and displays it using [Rapatas/CAVIS](https://github.com/Rapatas/CAVIS). Predator & Prey is simulating an environment where a predator (red) and a prey (green) exist in a harmonic balance using the following set of rules:
 * If a prey moves, it multiplies in the previous position
 * If a predator lands on a prey, it turns it into a predator
 * Predators die after 10 rounds
 
-![Preview](https://i.imgur.com/QzlzzWK.png "Preditor & Prey")
+# Usage
+```bash
+predator_and_prey_sfml --height 100
+```
 
-This project implements Predator & Prey using C++ and displays it using [Rapatas/CAVIS](https://github.com/Rapatas/CAVIS)
+![Preview](https://i.imgur.com/LzmDeJM.gif "Preditor & Prey")
 
-# Requirements
+# Installation
+
+## Dependencies
+Follow the project's installation instructions.
 * [Rapatas/flat_matrix](https://github.com/Rapatas/flat_matrix)
 * [Rapatas/CAVIS](https://github.com/Rapatas/CAVIS)
-* [SFML](https://www.sfml-dev.org/index.php)
+* [Taywee/args](https://github.com/Taywee/args)
+
+## Predator & Prey
+```bash
+git clone https://github.com/Rapatas/predator_and_prey_sfml
+mkdir predator_and_prey_sfml/build
+cd predator_and_prey_sfml/build
+cmake ..
+make install # As root
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,83 @@
 #include <CAVIS/window.h>
-#include <utility>
+#include <iostream>
+#include <args.hxx>
 
 #include "predator_and_prey.h"
 
-int main() {
+int main(int argc, char *argv[]) {
 
-	// HD aspect ratio = 16:9
-	unsigned height = 100;
-	unsigned width = height * 16 / 9;
-	unsigned pixel_size = 4;
+	args::ArgumentParser parser(
+		"Predator & Prey in SFML using Rapatas/CAVIS",
+		""
+	);
 
-	Window window(std::make_unique<PredatorAndPrey>(), width, height, pixel_size, "Predator & Prey");
+	args::HelpFlag help(
+		parser,
+		"help",
+		"Display this help menu",
+		{"help"}
+	);
+	args::ValueFlag<int> arg_width(
+		parser,
+		"cells",
+		"The width of the arena in cells",
+		{'w', "width"}
+	);
+	args::ValueFlag<int> arg_height(
+		parser,
+		"cells",
+		"The height of the arena in cells",
+		{'h', "height"}
+	);
+	args::ValueFlag<int> arg_cell_size(
+		parser,
+		"pixels",
+		"The size of a cell in pixels",
+		{'s', "cell-size"}
+	);
+
+	try {
+		parser.ParseCLI(argc, argv);
+	} catch (args::Help) {
+		std::cout << parser;
+		return 0;
+	} catch (args::ParseError e) {
+		std::cerr << e.what() << std::endl;
+		std::cerr << parser;
+		return 1;
+	} catch (args::ValidationError e) {
+		std::cerr << e.what() << std::endl;
+		std::cerr << parser;
+		return 1;
+	}
+
+	double hd_aspect_ratio = 16 / 9.0;
+	unsigned height    = args::get(arg_height);
+	unsigned width     = args::get(arg_width);
+	unsigned cell_size = args::get(arg_cell_size);
+
+	if (!arg_height && arg_width) {
+		height = args::get(arg_width) / hd_aspect_ratio;
+	}
+	if (!arg_width && arg_height) {
+		width = args::get(arg_height) * hd_aspect_ratio;
+	}
+	if (!arg_width && !arg_height) {
+		height = 100;
+		width = height * hd_aspect_ratio;
+	}
+
+	if (!arg_cell_size) {
+		cell_size = 4;
+	}
+
+	Window window(
+		std::make_unique<PredatorAndPrey>(),
+		width,
+		height,
+		cell_size,
+		"Predator & Prey"
+	);
 	window.run();
 
 	return 0;
